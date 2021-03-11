957d7fb298c4ac0d8312c2682632c31eef810a27
Mauro <m.feltrin@reply.it>
2021-03-11 20:10:29 +0100

    a

--- ReplyChallenge2021/ReplyChallenge2021/Program.cs
+++ ReplyChallenge2021/ReplyChallenge2021/Program.cs
@@ -12,70 +12,86 @@ namespace ReplyChallenge2021
         static void Main(string[] args)
         {
             //primo commmento di mauro
-            string filename = "data_scenarios_b_mumbai.in";
+            string filename = "data_scenarios_f_tokyo.in";
             City city = ReadFile.ReadInputFile(filename, 1, 5, 9, 1);
 
             int av = city.AvarageAntennasRange();
 
             //Ordinare punti mappa
-            List<Tuple<int,int,int>> listOfPoints = new List<Tuple<int,int,int>>();
+            List<Tuple<int,int,int>> listOfPointsOrdered = new List<Tuple<int,int,int>>();
 
-            for (int x = 0; x <= city.width ; x++)
+            Console.WriteLine("1");
+
+            city.antennas = city.antennas.OrderBy(a => a.speed).ToList();
+            city.buildings = city.buildings.OrderBy(b => b.speed).ToList();
+
+
+            for (int i = 0; i < city.antennas.Count(); i++)
             {
-                for (int y = 0; y <= city.heigth ; y++)
-                {
-                    List<Building> buildings = city.BuildingInsideRange(av, x, y);
-                    var pointValue = 0;
+                city.antennas[i].bestX = city.buildings[i].positionX;
+                city.antennas[i].bestY = city.buildings[i].positionY;
+            }
+
+            //for (int x = 0; x <= city.width ; x++)
+            //{
+            //    for (int y = 0; y <= city.heigth ; y++)
+            //    {
+            //        //List<Building> buildings = city.BuildingInsideRange(av, x, y);
+            //        //var pointValue = 0;
+
+            //        //pointValue = buildings.Sum(building => building.internalScore);
+
+            //        listOfPointsOrdered.Add(new Tuple<int,int,int>(x,y,0));
+            //    }
+            //}
+
+            //Console.WriteLine("2");
+
+            ////Lista ordinata
+            ////IEnumerable<Tuple<int,int,int>> listOfPointsOrdered = listOfPoints.OrderBy(point => point.Item3);
+
+            //IEnumerable<Antenna> antennasOrdered = city.antennas.OrderBy(antenna => antenna.internalValue);
+
+            //Console.WriteLine("3");
+
+            //foreach (Antenna antenna in antennasOrdered)
+            //{
+            //    Console.WriteLine(antenna.id);
+            //    int bestX = -1;
+            //    int bestY = -1;
+            //    int bestScore = -1;
+            //    foreach(Tuple<int,int,int> point in listOfPointsOrdered)
+            //    {
+            //        List<Building> buildings = city.BuildingInsideRange(av, point.Item1, point.Item2);
+            //        var singolarScore = antenna.CalculateScore(city, point, buildings);
                     
-                    pointValue = buildings.Sum(building => building.internalScore);
 
-                    listOfPoints.Add(new Tuple<int,int,int>(x,y,pointValue));
-                }
-            }
+            //        if (singolarScore > bestScore)
+            //        {
+            //            bestX = point.Item1;
+            //            bestY = point.Item2;
+            //            bestScore = singolarScore;
+            //        }
+            //    }
 
-            //Lista ordinata
-            IEnumerable<Tuple<int,int,int>> listOfPointsOrdered = listOfPoints.OrderBy(point => point.Item3);
+            //    //Assegno antenna e aggiorno mappa città
+            //    antenna.bestX = bestX;
+            //    antenna.bestY = bestY;
+            //    antenna.bestScore = bestScore;
+            //    city.positionAntenne[bestX, bestY] = 1;
 
-            IEnumerable<Antenna> antennasOrdered = city.antennas.OrderBy(antenna => antenna.internalValue);
+            //    //Aggiorno buildings
+            //    foreach(Building b in city.BuildingInsideRange(av, bestX, bestY))
+            //    {
+            //        var scoreForAntenna = b.ScoreForAntenna(antenna, bestX, bestY);
+            //        if (b.bestAntennaScore < scoreForAntenna)
+            //        {
+            //            b.bestAntennaScore = scoreForAntenna;
+            //        }
+            //    }
+            //}
 
-            foreach(Antenna antenna in antennasOrdered)
-            {
-                Console.WriteLine(antenna.id);
-                int bestX = -1;
-                int bestY = -1;
-                int bestScore = -1;
-                foreach(Tuple<int,int,int> point in listOfPointsOrdered)
-                {
-                    List<Building> buildings = city.BuildingInsideRange(av, point.Item1, point.Item2);
-                    var singolarScore = antenna.CalculateScore(city, point, buildings);
-                    //Console.WriteLine(singolarScore);
-
-                    if (singolarScore > bestScore)
-                    {
-                        bestX = point.Item1;
-                        bestY = point.Item2;
-                        bestScore = singolarScore;
-                    }
-                }
-
-                //Assegno antenna e aggiorno mappa città
-                antenna.bestX = bestX;
-                antenna.bestY = bestY;
-                antenna.bestScore = bestScore;
-                city.positionAntenne[bestX, bestY] = 1;
-
-                //Aggiorno buildings
-                foreach(Building b in city.BuildingInsideRange(av, bestX, bestY))
-                {
-                    var scoreForAntenna = b.ScoreForAntenna(antenna, bestX, bestY);
-                    if (b.bestAntennaScore < scoreForAntenna)
-                    {
-                        b.bestAntennaScore = scoreForAntenna;
-                    }
-                }
-            }
-
-            int finalScore = city.CalculateScore();
+            //int finalScore = city.CalculateScore();
 
             ReadFile.WriteOutputFile("out-" + filename, city);
             
